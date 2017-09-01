60fde0bf29a93caf93732fd745cd85df18289734
Michael Truog <mjtruog@gmail.com>
2017-09-01 02:58:39 -0700

    Add main changes for 1.7.2-rc2

--- alpine/mkimg.cloudi.sh
+++ alpine/mkimg.cloudi.sh
@@ -4,11 +4,26 @@ profile_cloudi() {
 	title="CloudI LiveCD"
 	desc="CloudI AlpineLinux LiveCD"
 	hostname="cloudi"
-	rootfs_size="335544320" # 256MB + 64MB
+	rootfs_size="765460480" # 730 MB
 	kernel_cmdline="nomodeset console=tty0 console=ttyS0,19200 rootflags=\"size=$rootfs_size\""
 	syslinux_serial="0 19200"
 	kernel_addons=""
-	apks="$apks cloudi"
 	apkovl="genapkovl-cloudi.sh"
+	apks="$apks cloudi"
+	# add programming languages supported on all architecturs
+	apks="$apks go nodejs openjdk8 perl php7 python3 ruby"
+	# add programming languages supported on some architectures
+	case "$ARCH" in
+	x86_64 | armhf)
+		apks="$apks ghc cabal"
+		;;
+	esac
+	case "$ARCH" in
+	x86 | armhf | s390x)
+		;;
+	*)
+		apks="$apks ocaml"
+		;;
+	esac
 }
 
